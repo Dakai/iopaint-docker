b61bb3aa894a6e61bc908917650f53bcf2a8d315
dakai <oudakai@gmail.com>
2024-06-19 20:39:05 +0800

    Update Dockerfile and package.json dependenciesfor Python and packages

--- run.sh
+++ run.sh
@@ -3,9 +3,7 @@
 python3 -m venv "$VIRTUAL_ENV"
 . "$VIRTUAL_ENV"/bin/activate
 pip3 install --upgrade pip
-# temp disable
-#pip3 install torch==2.1.0 torchvision==0.16.0 --extra-index-url https://download.pytorch.org/whl/cpu
-#pip3 install -r requirements.txt
-# temp disable
+pip3 install torch==2.1.0 torchvision==0.16.0 --extra-index-url https://download.pytorch.org/whl/cpu
+pip3 install -r requirements.txt
 
 bun ./build/index.js
